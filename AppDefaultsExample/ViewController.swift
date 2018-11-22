40da9ab85cf3dc77d9b8bc13360cd7df35ab87d9
Yuske Fukuyama <yuskefuku@gmail.com>
2018-11-22 20:54:20 +0900

    Initial Commit

---
+++ AppDefaultsExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppDefaultsExample
+//
+//  Created by Yuske Fukuyama on 2018/11/22.
+//  Copyright © 2018 Yuske Fukuyama. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
